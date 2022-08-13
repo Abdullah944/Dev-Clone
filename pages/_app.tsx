c1771f92318d74ae0dbe86422f7a4bcbf239b43d
Abdullah90 <darkside944@gmail.com>
2022-08-13 22:54:49 +0300

    Add main Toaster component to use it in all app

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,7 @@
 import "../styles/globals.css";
 import type { AppProps } from "next/app";
 import Navbar from "../components/Navbar";
+import { Toaster } from "react-hot-toast";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
@@ -13,6 +14,7 @@ function MyApp({ Component, pageProps }: AppProps) {
         username={""}
       />
       <Component {...pageProps} />
+      <Toaster />
     </>
   );
 }
