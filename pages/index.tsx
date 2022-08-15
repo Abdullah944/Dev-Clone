05ac3a88ae0a9e56cec5bc17e02b95e522889395
Abdullah90 <darkside944@gmail.com>
2022-08-15 23:01:32 +0300

    Delete unused imports

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,6 @@
 import type { NextPage } from "next";
-import Head from "next/head";
-import Image from "next/image";
 import toast from "react-hot-toast";
 import Loader from "../components/Loader";
-import styles from "../styles/Home.module.css";
 
 const Home: NextPage = () => {
   return (
