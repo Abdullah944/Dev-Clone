43a63c3fbd7019a596769f6a2e47bb650ae91b29
Abdullah90 <darkside944@gmail.com>
2022-08-17 01:28:29 +0300

    Use the hooks & send all user data

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,13 +3,14 @@ import type { AppProps } from "next/app";
 import Navbar from "../components/Navbar";
 import { Toaster } from "react-hot-toast";
 import { UserContext } from "../lib/context";
+import { useUserData } from "../lib/hooks";
 
 function MyApp({ Component, pageProps }: AppProps) {
+  //*  This is costume hook to get all the user from firebaseStore & we can use it in all app as props
+  const userData = useUserData();
   return (
     <>
-      <UserContext.Provider
-        value={{ user: {}, username: "abdullah", photoURL: "" }}
-      >
+      <UserContext.Provider value={userData}>
         {/* Add the NavBar for all the components */}
         <Navbar />
         <Component {...pageProps} />
