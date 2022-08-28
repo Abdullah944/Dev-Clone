5c83f6c8a563a42173c06d0c795d2f8a0210edbf
Abdullah90 <darkside944@gmail.com>
2022-08-28 10:21:36 +0300

    Show the user info + Posts

--- pages/[username]/index.tsx
+++ pages/[username]/index.tsx
@@ -1,3 +1,39 @@
-export default function Page({}) {
-  return <main></main>;
+import { getUserWithUsername, postToJSON } from "../../lib/firebase";
+import UserProfile from "../../components/UserProfile";
+import PostFeed from "../../components/PostFeed";
+
+export async function getServerSideProps({ query }: any) {
+  const { username } = query;
+
+  const userDoc = await getUserWithUsername(username);
+
+  // JSON serializable data
+  let user = null;
+  let posts = null;
+
+  if (userDoc) {
+    user = userDoc.data();
+    const postsQuery = userDoc.ref
+      .collection("posts")
+      .where("published", "==", true)
+      .orderBy("createdAt", "desc")
+      .limit(5);
+    posts = (await postsQuery.get()).docs.map(postToJSON);
+  }
+
+  return {
+    props: { user, posts }, // will be passed to the page component as props
+  };
+}
+type UserProfilePageProps = {
+  user: any;
+  posts: any;
+};
+export default function UserProfilePage({ user, posts }: UserProfilePageProps) {
+  return (
+    <main>
+      <UserProfile user={user} />
+      <PostFeed posts={posts} post={""} admin={false} />
+    </main>
+  );
 }
